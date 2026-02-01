85064d6c7ae3d008e34d11ad6d2e195b9bc48a04
NicDole <nicodolgopolyy@gmail.com>
2026-02-01 13:47:56 -0500

    interpreter.c V1 with Echo my_touch my_cd

--- Part1/interpreter.c
+++ Part1/interpreter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>   // chdir
 #include "shellmemory.h"
 #include "shell.h"
 
@@ -17,12 +18,25 @@ int badcommandFileDoesNotExist() {
     return 3;
 }
 
+int badcommandMyCd() {
+    printf("Bad command: my_cd\n");
+    return 1;
+}
+
+int badcommandMyTouch() {
+    printf("Bad command: my_touch\n");
+    return 1;
+}
+
 int help();
 int quit();
 int set(char *var, char *value);
 int print(char *var);
 int source(char *script);
-int badcommandFileDoesNotExist();
+
+int echo_cmd(char *token);
+int my_touch(char *filename);
+int my_cd(char *dirname);
 
 // Interpret commands and their arguments
 int interpreter(char *command_args[], int args_size) {
@@ -37,46 +51,53 @@ int interpreter(char *command_args[], int args_size) {
     }
 
     if (strcmp(command_args[0], "help") == 0) {
-        //help
-        if (args_size != 1)
-            return badcommand();
+        if (args_size != 1) return badcommand();
         return help();
 
     } else if (strcmp(command_args[0], "quit") == 0) {
-        //quit
-        if (args_size != 1)
-            return badcommand();
+        if (args_size != 1) return badcommand();
         return quit();
 
     } else if (strcmp(command_args[0], "set") == 0) {
-        //set
-        if (args_size != 3)
-            return badcommand();
+        if (args_size != 3) return badcommand();
         return set(command_args[1], command_args[2]);
 
     } else if (strcmp(command_args[0], "print") == 0) {
-        if (args_size != 2)
-            return badcommand();
+        if (args_size != 2) return badcommand();
         return print(command_args[1]);
 
     } else if (strcmp(command_args[0], "source") == 0) {
-        if (args_size != 2)
-            return badcommand();
+        if (args_size != 2) return badcommand();
         return source(command_args[1]);
 
-    } else
+    } else if (strcmp(command_args[0], "echo") == 0) {
+        if (args_size != 2) return badcommand();
+        return echo_cmd(command_args[1]);
+
+    } else if (strcmp(command_args[0], "my_touch") == 0) {
+        if (args_size != 2) return badcommandMyTouch();
+        return my_touch(command_args[1]);
+
+    } else if (strcmp(command_args[0], "my_cd") == 0) {
+        if (args_size != 2) return badcommandMyCd();
+        return my_cd(command_args[1]);
+
+    } else {
         return badcommand();
+    }
 }
 
 int help() {
-
-    // note the literal tab characters here for alignment
-    char help_string[] = "COMMAND			DESCRIPTION\n \
-help			Displays all the commands\n \
-quit			Exits / terminates the shell with “Bye!”\n \
-set VAR STRING		Assigns a value to shell memory\n \
-print VAR		Displays the STRING assigned to VAR\n \
-source SCRIPT.TXT	Executes the file SCRIPT.TXT\n ";
+    char help_string[] =
+        "COMMAND\t\t\tDESCRIPTION\n \
+help\t\t\tDisplays all the commands\n \
+quit\t\t\tExits / terminates the shell with “Bye!”\n \
+set VAR STRING\t\tAssigns a value to shell memory\n \
+print VAR\t\tDisplays the STRING assigned to VAR\n \
+source SCRIPT.TXT\tExecutes the file SCRIPT.TXT\n \
+echo TOKEN\t\tDisplays TOKEN or variable value\n \
+my_touch FILE\t\tCreates an empty file\n \
+my_cd DIR\t\tChanges the current directory\n ";
     printf("%s\n", help_string);
     return 0;
 }
@@ -87,28 +108,75 @@ int quit() {
 }
 
 int set(char *var, char *value) {
-    // Challenge: allow setting VAR to the rest of the input line,
-    // possibly including spaces.
-
-    // Hint: Since "value" might contain multiple tokens, you'll need to loop
-    // through them, concatenate each token to the buffer, and handle spacing
-    // appropriately. Investigate how `strcat` works and how you can use it
-    // effectively here.
-
     mem_set_value(var, value);
     return 0;
 }
-
 
 int print(char *var) {
     printf("%s\n", mem_get_value(var));
     return 0;
 }
 
+int echo_cmd(char *token) {
+    // One token only:
+    // - if token starts with '$', echo prints the variable's value, or a blank line if not found
+    // - otherwise echo prints the token
+    if (token == NULL) {
+        printf("\n");
+        return 0;
+    }
+
+    if (token[0] == '$') {
+        char *varname = token + 1;
+        if (varname[0] == '\0') {
+            printf("\n");
+            return 0;
+        }
+
+        char *val = mem_get_value(varname);
+
+        // starter mem_get_value returns this literal when not found
+        if (strcmp(val, "Variable does not exist") == 0) {
+            printf("\n");
+        } else {
+            printf("%s\n", val);
+            free(val); // mem_get_value strdup()s successful lookups
+        }
+        return 0;
+    }
+
+    printf("%s\n", token);
+    return 0;
+}
+
+int my_touch(char *filename) {
+    if (filename == NULL || filename[0] == '\0') {
+        return badcommandMyTouch();
+    }
+
+    FILE *f = fopen(filename, "a"); // create if doesn't exist
+    if (f == NULL) {
+        return badcommandMyTouch();
+    }
+    fclose(f);
+    return 0;
+}
+
+int my_cd(char *dirname) {
+    if (dirname == NULL || dirname[0] == '\0') {
+        return badcommandMyCd();
+    }
+
+    if (chdir(dirname) != 0) {
+        return badcommandMyCd();
+    }
+    return 0;
+}
+
 int source(char *script) {
     int errCode = 0;
     char line[MAX_USER_INPUT];
-    FILE *p = fopen(script, "rt");      // the program is in a file
+    FILE *p = fopen(script, "rt");
 
     if (p == NULL) {
         return badcommandFileDoesNotExist();
@@ -116,7 +184,7 @@ int source(char *script) {
 
     fgets(line, MAX_USER_INPUT - 1, p);
     while (1) {
-        errCode = parseInput(line);     // which calls interpreter()
+        errCode = parseInput(line); // which calls interpreter()
         memset(line, 0, sizeof(line));
 
         if (feof(p)) {
@@ -126,6 +194,5 @@ int source(char *script) {
     }
 
     fclose(p);
-
     return errCode;
 }
